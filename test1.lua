c92c136d4cc9f3d79e5003632bbb7442062df53e
phucvovipbro-cmd <phucvovipbro@gmail.com>
2026-05-08 03:22:15 +0700

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -1,35 +1,8 @@
-local UIS = game:GetService("UserInputService")
-local Players = game:GetService("Players")
+-- Chạy trong LocalScript
+local playerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-local player = Players.LocalPlayer
-local pg = player:WaitForChild("PlayerGui")
-
-local gui = Instance.new("ScreenGui")
-gui.Parent = pg
-
-local label = Instance.new("TextLabel")
-label.Parent = gui
-label.Size = UDim2.new(1,0,0,40)
-label.BackgroundColor3 = Color3.new(0,0,0)
-label.TextColor3 = Color3.new(1,1,1)
-label.TextScaled = true
-label.Text = "Tap GUI để inspect"
-
-local function scan()
-    for _,v in pairs(pg:GetDescendants()) do
-        if v:IsA("GuiObject") then
-            
-            v.InputBegan:Connect(function(input)
-                if input.UserInputType == Enum.UserInputType.Touch then
-                    
-                    label.Text = v:GetFullName()
-                    warn(v:GetFullName())
-                    
-                end
-            end)
-            
-        end
+for _, gui in ipairs(playerGui:GetDescendants()) do
+    if gui:IsA("TextButton") and gui.Text == "PLAY NOW" then
+        print(gui:GetFullName()) -- In ra đường dẫn đầy đủ
     end
 end
-
-scan()
